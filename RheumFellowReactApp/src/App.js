7328e9879f5dbf35a6dd08660d674ae033724ab2
Bahtiyartoz <bahtiyartoz@gmail.com>
2025-05-24 14:27:18 -0400

    Add files via upload

---
+++ RheumFellowReactApp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <main style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>RheumFellow.com</h1>
+      <p>This is a placeholder homepage for the RheumFellow educational platform.</p>
+    </main>
+  );
+}
+
+export default App;
